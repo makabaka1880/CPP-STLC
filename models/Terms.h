5f9850b5a281fedee23f1802ee76d2919fedf198
makabaka1880 <makabaka1880@outlook.com>
2025-08-03 20:19:04 +0800

    feat:updated typing and typo in Variable.h

--- models/Terms.h
+++ models/Terms.h
@@ -5,6 +5,8 @@
 #ifndef TERM_H
 #define TERM_H
 
+#include "Type.h"
+
 #include <string>
 #include <memory>
 #include <iostream>
@@ -16,6 +18,7 @@ public:
     [[nodiscard]] virtual std::unique_ptr<Term> substitute(std::string target, Term& newValue) const = 0;
     [[nodiscard]] virtual std::unique_ptr<Term> beta_reduce() const = 0;
     [[nodiscard]] virtual std::unique_ptr<Term> clone() const = 0;
+    [[nodiscard]] virtual std::unique_ptr<Type> type const = 0;
     [[nodiscard]] virtual bool is_normal() const = 0;
     [[nodiscard]] virtual bool has_free(std::string target) const = 0;
     [[nodiscard]] virtual std::string to_string() const = 0;
--- models/terms/Variable.h
+++ models/terms/Variable.h
@@ -9,6 +9,8 @@
 
 
 class Variable final : public Term {
+protected:
+    std::unique_ptr<Type> type;
 public:
     std::string name;
 
@@ -19,9 +21,9 @@ public:
     [[nodiscard]] std::unique_ptr<Term> alpha_convert(std::string newValue) const override;;
     [[nodiscard]] std::unique_ptr<Term> beta_reduce() const override;
     [[nodiscard]] std::unique_ptr<Term> clone() const override;
+    [[nodiscard]] std::string to_string() const override;
     [[nodiscard]] bool is_normal() const override;
     [[nodiscard]] bool has_free(std::string target) const override;
-    [[nodiscard]] virtual std::string to_string() const override;
 };
 
 #endif //VARIABLE_H
